2b9f8d5098204c7452b5c935f25a049bcf09e6b4
mateusribeirocampos <mateusrcampos@gmail.com>
2025-08-16 06:25:11 -0300

    chore: updated

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["src/content/blog/en/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fusing-mdx.mdx&astroContentModuleFlag=true")],
-["src/content/blog/pt/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpt%2Fusing-mdx.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/pt/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpt%2Fusing-mdx.mdx&astroContentModuleFlag=true")],
+["src/content/blog/en/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fusing-mdx.mdx&astroContentModuleFlag=true")]]);
 		
